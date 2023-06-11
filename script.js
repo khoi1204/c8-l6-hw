3acc18a7fdf46f9da5515d0b851abaee089ccde4
khoi1204 <127846687+khoi1204@users.noreply.github.com>
2023-06-11 22:36:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+// class Book {
+//   constructor(title, author, publicationYear, genre){
+//     this.title = title;
+//     this.author = author;
+//     this.publicationYear = publicationYear;
+//     this.genre = genre;
+//   }
+
+//   showInfo(){
+//     console.log(`Title:  ${this.title}, Author:  ${this.author}, PublicationYear:  ${this.publicationYear}, Genre:  ${this.genre}`);
+//   }
+
+//   checkNewBook(){
+//     if (this.publicationYear >= 2021){
+//       console.log(`Sách ${this.title} là sách mới`);
+//     }
+//     else{
+//       console.log(`Sách ${this.title} không phải là sách mới`);
+//     }
+//   }
+
+//   checkClassic(){
+//     if (this.publicationYear < 1990){
+//       console.log(`Sách ${this.title} là một sách cổ điển`);
+//     }
+//     else{
+//       console.log(`Sách ${this.title} không phải là sách cổ điển`);
+//     }
+//   }
+// }
+
+// const books = [
+//   new Book('To Kill a Mockingbird', 'Harper Lee', 1960, 'Fiction'),
+//   new Book('1984', 'George Orwell', 1949, 'Dystopian'),
+//   new Book('The Great Gatsby', 'F. Scott Fitzgerald', 1925, 'Classic'),
+//   new Book('Pride and Prejudice', 'Jane Austen', 1813, 'Romance'),
+// ];
+
+// for (let index of books){
+//   index.showInfo()
+//   index.checkNewBook()
+//   index.checkClassic()
+// }
+
+
+
+
